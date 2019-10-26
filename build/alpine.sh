84ac385f797976ea208fdc3b62360d651e636fcf
Kalyane Lordão <kalyane.lordao@lavid.ufpb.br>
2019-10-26 12:09:19 -0400

    Update alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -2,6 +2,7 @@ echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && 
 echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
 apk update && \
+ && apk add --no-cache \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
