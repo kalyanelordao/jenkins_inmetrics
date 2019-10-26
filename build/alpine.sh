9a101431e723cd37125ca66d47c7b7bbe319cbf7
Kalyane Lordao <KMLord@MacBook-Pro-de-Kalyane.local>
2019-10-26 11:55:21 -0400

    change alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -5,6 +5,5 @@ apk update && \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
-    postgresql-dev \
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
